01c61dd06ba3e5d1515f722f7c943e92511fb6a7
Maadeha shaikh <maadeham@gmail.com>
2024-03-29 05:16:10 +0500

    something new added

--- my-basic-app/src/App.js
+++ my-basic-app/src/App.js
@@ -1,3 +1,4 @@
+
 import logo from './logo.svg';
 import './App.css';
 import News from './commponent/news';
@@ -6,7 +7,57 @@ function App() {
   const [count , setCount] = useState(0);
   return (
    <>
-   <div className='main_heading'>
+   {/* Nav bar here  */}
+   <div className='navbar'>
+    <div className='logo'>
+    <img src='https://s.yimg.com/zb/imgv1/04eb2405-7ccf-3059-96e6-83c3cc778823/t_500x300'></img>
+    </div>
+    <div className='navs'>
+      <ul>
+        <a>SALE</a>
+        <a>NEW IN</a>
+        <a className='special'>#INSTAKHAADI</a>
+        <a>FABRICS</a>
+        <a>READY TO WEAR</a>
+      </ul>
+      </div>
+      <div className='icons'>
+      <i class="fa-solid fa-magnifying-glass"></i>
+      <i class="fa-regular fa-heart"></i>
+      <i class="fa-regular fa-address-book"></i>
+      <i class="fa-solid fa-bag-shopping"></i>
+      </div>
+   </div>
+   {/* -------------------------------- */} 
+
+   <div className='main_page'>
+    <img className='first_img' src='https://pk.khaadi.com/on/demandware.static/-/Sites-khaadi-master-catalog/default/dweda1df0a/images/hi-res/eet24369_d-purple_2.jpg'></img>
+    <img className='second_img' src='https://pk.khaadi.com/on/demandware.static/-/Sites-khaadi-master-catalog/default/dw11fcc61c/images/hi-res/eeb24324_black_2.jpg'></img>
+   </div>
+   <div className='main_div_content'><h1>Chunari</h1> <br/><h2> edits</h2><br/> <span> Ready To Wear Eid Collection</span> </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   {/* <div className='main_heading'>
     <h1>Welcome to The Best News Website !</h1>
     <h4>Where you'll explore the see of information all around the world !!! </h4>
     </div>
@@ -20,7 +71,7 @@ function App() {
   
     <News title={"News Five"} image={"https://images.unsplash.com/photo-1457369804613-52c61a468e7d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG5ld3NwYXBlciUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D"}></News>
 
-    <News title={"News Six"} image={"https://media.istockphoto.com/id/1192070239/photo/abstract-digital-news-concept.jpg?s=1024x1024&w=is&k=20&c=hf3O9HZpnn5RoomntLIxFT7uwBqFwTXulr-Z3pPTZGA="}></News>
+    <News title={"News Six"} image={"https://media.istockphoto.com/id/1192070239/photo/abstract-digital-news-concept.jpg?s=1024x1024&w=is&k=20&c=hf3O9HZpnn5RoomntLIxFT7uwBqFwTXulr-Z3pPTZGA="}></News> */}
 </>
     
   );
--- my-basic-app/src/commponent/news.jsx
+++ my-basic-app/src/commponent/news.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 const News = (props) => {
+  const[likes,setLikes] = useState(0);
+  function icreasing_likes (){
+    setLikes(likes+1);
+  }
   return (
     <>
     <div className='news-div'>
     <img src={props.image}/>
     <h1>{props.title}</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quae quibusdam soluta in quas eveniet. Libero saepe ullam suscipit fuga adipisci unde dolores, doloremque non reprehenderit molestias perferendis quasi necessitatibus.</p>
+    <button onClick={icreasing_likes}>Like{likes}</button>
     <button className='btn'>READ MORE</button>
     </div>
     </>
